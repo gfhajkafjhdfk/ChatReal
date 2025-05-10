261d1db14a5b1bd1b4fba9042e911b6d02d89d13
gfhajkafjhdfk <166894544+gfhajkafjhdfk@users.noreply.github.com>
2025-05-10 13:43:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+      setTimeout(() => {
+        document.getElementById('loading-screen').style.display = 'none';
+        document.getElementById('termsPage').style.display = 'block';
+      }, 3000);
+    });
 
+// フェードイン関係
+    const featureItems = document.querySelectorAll('.feature-item');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  featureItems.forEach(item => {
+    observer.observe(item);
+  });
+    
