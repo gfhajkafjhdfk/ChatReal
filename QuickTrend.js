9561c0ce7f2582b72e7654ed87c68e4333262c14
gfhajkafjhdfk <166894544+gfhajkafjhdfk@users.noreply.github.com>
2025-05-10 14:17:44 +0900

    Update QuickTrend.js

--- QuickTrend.js
+++ QuickTrend.js
@@ -1,1 +1,28 @@
+//ジャンル選択をキャプチャし、ジャンルリストページに移動する（ログイン用）
 
+        document.getElementById('genreForm').addEventListener('submit', function(e) {
+            e.preventDefault();
+             const selectedGenres = [];//ここから
+            document.querySelectorAll('input[name="genre"]:checked').forEach(genre => {
+                selectedGenres.push(genre.value);
+            });
+            console.log('Selected Genres:', selectedGenres);//ここまで
+            genrePage.style.display = 'none';
+            quicktrendPage.style.display = 'block';
+        });//（ログイン用ここまで）
+//titleについてのアニメーション
+window.onload = function() {
+  const titleElements = document.querySelectorAll('.title'); // 複数の要素に対応
+
+  titleElements.forEach(titleElement => {
+    const text = titleElement.textContent;
+    titleElement.textContent = ''; // 元のテキストをクリア
+
+    for (let i = 0; i < text.length; i++) {
+      const span = document.createElement('span');
+      span.textContent = text[i];
+      span.style.animation = `fadeInUpChar 1.5s ease-out ${i * 0.1}s forwards`; // アニメーションと遅延
+      titleElement.appendChild(span);
+    }
+  });
+};
